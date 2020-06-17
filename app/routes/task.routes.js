cf317bbe50c93c276da14770ed0d7522cfa61346
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-06-17 00:57:21 -0400

    rutas task

---
+++ app/routes/task.routes.js
@@ -0,0 +1,15 @@
+module.exports = app => {
+    const tasks = require("../controllers/task.controller");
+    var router = require("express").Router();
+  
+    router.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+    app.use('/users', router); //Not sure
+  
+    // Create new user
+    router.post("/createTask", tasks.createTask);
+  };
+  

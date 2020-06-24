733694f62c0c3acccf4b79b3afcbd48045e5b857
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-06-23 21:46:45 -0400

    soy bruta y media

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,11 +1,11 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/auth.controller");
 
-/**
+
  module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
@@ -20,29 +20,5 @@ const controller = require("../controllers/auth.controller");
 
   app.post("/users/signin", controller.signin);
 };
- */
+ 
 
-module.exports = app => {
-  const users = require("../controllers/user.controller");
-  var router = require("express").Router();
-
-  router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  app.use('/users', router);
-  
-  router.post(
-    "/signup",
-    [
-      verifySignUp.checkDuplicateEmail,
-      verifySignUp.checkRolesExisted
-    ],
-    users.signup
-  );
-
-  router.post("/signin", users.signin);
-  
-  
-};
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,6 +1,29 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/user.controller");
 
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  //app.get("/api/test/all", controller.allAccess);
+
+  app.get(
+    "/users/calendar", //ruta del calendario aquí
+    [authJwt.verifyToken]
+    //controller.userBoard
+  );
+
+  app.get(
+    "/admin", //ruta del dashboard de admin aqui
+    [authJwt.verifyToken, authJwt.isAdmin]
+    //controller.adminBoard
+  );
+};
+
 /**
  module.exports = app => {
   const users = require("../controllers/user.controller");
@@ -21,53 +44,9 @@ const controller = require("../controllers/user.controller");
 };
  */
 
-module.exports = app => {
-  //const users = require("../controllers/user.controller");
-  var router = require("express").Router();
 
-  router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  app.use('/users', router);
-  
-  router.get(
-    "/calendar", //ruta del calendario aquí
-    [authJwt.verifyToken]
-  );
 
-  router.get(
-    "/admin", //ruta del dashboard de admin aqui
-    [authJwt.verifyToken, authJwt.isAdmin]
-  );
-  
-  
-};
 
-/**
-  module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
-    next();
-  });
-
-  //app.get("/api/test/all", controller.allAccess);
-
-  app.get(
-    "/users/calendar", //ruta del calendario aquí
-    [authJwt.verifyToken]
-    //controller.userBoard
-  );
-
-  app.get(
-    "/admin", //ruta del dashboard de admin aqui
-    [authJwt.verifyToken, authJwt.isAdmin]
-    //controller.adminBoard
-  );
-};
- */
 
 
 

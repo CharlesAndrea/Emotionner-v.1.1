639cbcc944186dfdcf25d5286236af5660bc0bb6
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-06-23 21:48:25 -0400

    *

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://emotionner.web.app",
+  origin: "*",
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSucessStatus": 204,
